4002d531f8686f90d45c608101b0c6140723f830
jykim <kimorkim@gmail.com>
2017-05-15 21:29:18 +0900

    Add textarea style

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,5 +1,8 @@
 <template>
-  <textarea>
+  <textarea
+  rows="1"
+    style="white-space: pre; overflow: scroll;" wrap="off"
+  >
   </textarea>
 </template>
 
@@ -9,5 +12,24 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  textarea {
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid rgb(224, 224, 224);
+    font-size: 13px;
+    min-height: 20px;
+    height: 20px;
+    overflow: hidden;
+    resize: none;
+    -webkit-user-select: none;
+    -webkit-tap-highlight-color: transparent;
+
+    white-space:nowrap;
+  overflow:scroll;
+
+    &:focus {
+      outline: none;
+    }
+  }
 </style>
